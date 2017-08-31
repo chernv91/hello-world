5e887b1d2a6a4eccc964f37e1f193bdaa9ded25d
chernv91 <31442185+chernv91@users.noreply.github.com>
2017-08-31 22:58:57 +0200

    Add files via upload

---
+++ run.php
@@ -0,0 +1,16 @@
+<?php
+	function checkParenthesis($str) {
+		if (!is_null($str)) {
+			$cnt1 = substr_count($str, "(");
+			$cnt2 = substr_count($str, ")");
+			if ($cnt1 == $cnt2) {
+				return "Yes";
+			}
+			return "No";
+		}
+		return "No string for checking. Enter argument";
+	}
+	
+	$arr = $_SERVER['argv'];
+	echo checkParenthesis($arr[1]);
+?>
